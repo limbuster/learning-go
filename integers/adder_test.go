6537a804c9ef1064052579a10b8c4bfc1c11a25e
Kumar Limbu <kumarlimbu@gmail.com>
2020-07-18 14:50:53 +0800

    added: function to add integers

---
+++ integers/adder_test.go
@@ -0,0 +1,27 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func ExampleAdd2() {
+	sum := Add(1, -5)
+	fmt.Println(sum)
+	// Output: -4
+}
