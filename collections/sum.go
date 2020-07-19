e6fe554ac7d7d4cb57e5dd4dbed1ee0f3cada3e0
Kumar Limbu <kumarlimbu@gmail.com>
2020-07-19 13:32:07 +0800

    added: Sum of slices and sum of slices tail (without first element)

--- collections/sum.go
+++ collections/sum.go
@@ -8,3 +8,23 @@ func Sum(nums []int) int {
 	}
 	return result
 }
+
+// SumAll returns slice of sum of the slices passed as parameters
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails returns slice of sum of the slices passed as parameters except for the first element
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+}
